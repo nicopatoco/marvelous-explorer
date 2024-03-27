9a209c5958eda91348bb21b310b99fa34dd7bf57
nicopatoco <nicolasherrera.cp@gmail.com>
2024-03-27 18:52:41 +0100

    chore: add dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    PUBLIC_MARVEL_API_KEY: process.env.PUBLIC_MARVEL_API_KEY,
+    PRIVATE_MARVEL_API_KEY: process.env.PRIVATE_MARVEL_API_KEY,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
