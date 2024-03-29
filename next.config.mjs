143a8d9725b63711796c342f9f2cd206a5e6da6f
nicopatoco <nicolasherrera.cp@gmail.com>
2024-03-29 18:14:16 +0100

    fix: using context from layout

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['i.annihil.us'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.annihil.us',
+      },
+    ],
   },
   env: {
     PUBLIC_MARVEL_API_KEY: process.env.PUBLIC_MARVEL_API_KEY,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,52 @@
+/* eslint-disable @next/next/no-img-element */
 'use client'
-import { Provider } from 'react-redux'
-import { store } from './state/store'
-import Header from './components/Header'
-import CharactersPage from './(pages)/page'
+import { useMemo, useState } from 'react'
+import useCharacters from './hooks/useCharacters'
+import LoadingDisplay from './components/LoadingDisplay'
+import ErrorDisplay from './components/ErrorDisplay'
+import CharacterList from './components/CharacterList'
 
 export default function Home() {
+  const { characters, loading, error } = useCharacters()
+  const [filter, setFilter] = useState<string | undefined>(undefined)
+
+  const filteredCharacters = useMemo(() => {
+    if (filter) {
+      return characters.filter((character) => character.name.toLowerCase().includes(filter.toLowerCase()))
+    }
+    return characters
+  }, [characters, filter])
+
+  if (loading) {
+    return <LoadingDisplay />
+  }
+
+  if (error) {
+    return <ErrorDisplay error={error} />
+  }
+
   return (
-    <Provider store={store}>
-      <Header />
-      <main className="bg-white">
-        <CharactersPage />
-      </main>
-    </Provider>
+    <main>
+      {characters?.length > 0 && (
+        <>
+          <div className="flex flex-col p-12 xg:p-4 md:p-8 sm:p-4">
+            <div className="flex flex-row justify-start gap-4">
+              <img src="/search.svg" alt="search logo" className="customSearchImg" />
+              <input
+                className="w-full uppercase focus:outline-none"
+                type="text"
+                onChange={(e) => setFilter(e.target.value)}
+                placeholder="SEARCH A CHARACTER..."
+              />
+            </div>
+            <div className="my-4 border-t border-black"></div>
+            <div className="text-black">{filteredCharacters.length} RESULTS</div>
+            <div className="pt-4">
+              <CharacterList characters={filteredCharacters} />
+            </div>
+          </div>
+        </>
+      )}
+    </main>
   )
 }
