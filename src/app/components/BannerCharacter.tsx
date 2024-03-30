0b9b994fb0556579361f4427ec1d815af0a75ddb
nicopatoco <nicolasherrera.cp@gmail.com>
2024-03-30 19:42:40 +0100

    style: details page to be responsive

--- src/app/components/BannerCharacter.tsx
+++ src/app/components/BannerCharacter.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
-import { Character } from '../types/character'
 import { useDispatch, useSelector } from 'react-redux'
-import { AppDispatch, RootState } from '../state/store'
 import { addFavourite, removeFavourite } from '../state/charactersSlice'
+import { AppDispatch, RootState } from '../state/store'
+import { Character } from '../types/character'
 
 interface Props {
   character: Character
@@ -11,42 +11,41 @@ interface Props {
 export default function BannerCharacter({ character }: Props) {
   const favourites = useSelector((state: RootState) => state.characters.favourites)
   const dispatch = useDispatch<AppDispatch>()
+
   return (
-    <div className="w-full text-white bg-black rounded-br-xl">
-      <div className="flex flex-row">
-        <div className="flex w-3/12 min-w-[320px] max-w-[320px]">
+    <div className="w-full text-white bg-black rounded-br-xl sm:pb-8">
+      <div className="flex flex-col md:flex-row ">
+        <div className="flex w-full md:w-3/12 justify-center md:min-w-[320px] md:max-w-[320px] mb-0 bg-white">
           <Image
             src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
             alt={`Character ${character.id}`}
             width={320}
             height={320}
+            className="w-full md:w-auto"
           />
         </div>
-        <div className="flex flex-col justify-center w-9/12 pl-16 pr-8">
-          <div className="flex flex-row justify-between">
-            <div className="mb-8 text-4xl">{character.name}</div>
-            <div className="justify-center">
-              <div className="flex flex-row items-center gap-2">
-                {favourites.find((f) => f === character.id.toString()) ? (
-                  <Image
-                    src="/state-selected.png"
-                    alt="state selected"
-                    width={24}
-                    height={21}
-                    onClick={() => dispatch(removeFavourite(character.id.toString()))}
-                  />
-                ) : (
-                  <Image
-                    src="/state-unselected.png"
-                    alt="state unselected"
-                    width={24}
-                    height={21}
-                    onClick={() => dispatch(addFavourite(character.id.toString()))}
-                  />
-                )}
-
-                <div className="text-white">{favourites.length}</div>
-              </div>
+        <div className="flex flex-col justify-center w-full px-8 md:w-9/12 md:pl-16 md:pr-8">
+          <div className="flex flex-row justify-between py-4 md:py-8">
+            <div className="mt-4 text-2xl text-left md:text-4xl md:mt-8">{character.name}</div>
+            <div className="mt-4 md:mt-8">
+              {favourites.find((f) => f === character.id.toString()) ? (
+                <Image
+                  src="/state-selected.png"
+                  alt="state selected"
+                  width={24}
+                  height={21}
+                  onClick={() => dispatch(removeFavourite(character.id.toString()))}
+                  className=" min-w-[24px] max-w-[21px]"
+                />
+              ) : (
+                <Image
+                  src="/state-unselected.png"
+                  alt="state unselected"
+                  width={24}
+                  height={21}
+                  onClick={() => dispatch(addFavourite(character.id.toString()))}
+                />
+              )}
             </div>
           </div>
           <div className="text-base">{character.description}</div>
--- src/app/components/ComicCarousel.tsx
+++ src/app/components/ComicCarousel.tsx
@@ -8,19 +8,22 @@ interface Props {
 export default function ComicCarousel({ comics }: Props) {
   return (
     <>
-      <div className="container px-8">
+      <div className="container px-4 sm:px-8">
         <div className="my-8 font-bold">COMICS</div>
-        <div className="flex flex-row gap-4 overflow-x-auto">
+        <div className="flex flex-row gap-2 overflow-x-auto sm:gap-4">
           {comics &&
             comics.map((comic) => (
-              <div key={`comic-${comic.id}`} className="flex flex-col items-center min-w-[179.2px] max-w-[179.2px]">
+              <div
+                key={`comic-${comic.id}`}
+                className="flex flex-col items-center min-w-[45%] md:min-w-[14%] lg:min-w-[179.2px] max-w-[179.2px]"
+              >
                 <Image
                   src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
                   alt={`Comic ${comic.id}`}
                   width={179.2}
                   height={268.8}
                 />
-                <div className="mt-2 text-sm text-left">{comic.title}</div>
+                <div className="mt-2 text-xs text-center sm:text-sm sm:text-left">{comic.title}</div>
               </div>
             ))}
         </div>
