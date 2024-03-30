e41b00aa49eaa9cb1e1775dd5de8f8dd7efbfc1e
nicopatoco <nicolasherrera.cp@gmail.com>
2024-03-30 22:11:17 +0100

    feat: minimizes and optimizes assets

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,15 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      // Production-specific configurations
+      // Next.js automatically minimizes and optimizes your assets
+      // This is true by default in production
+      config.optimization.minimize = true
+    }
+    return config
+  },
   env: {
     PUBLIC_MARVEL_API_KEY: process.env.PUBLIC_MARVEL_API_KEY,
     PRIVATE_MARVEL_API_KEY: process.env.PRIVATE_MARVEL_API_KEY,
